f3535738d0f14e3a6be4016762c1d16ea44dff1f
Anton Krylov <anthony.kryloff@gmail.com>
2019-02-20 20:19:56 +0300

    fix example, add simple README

--- laser-blink.py
+++ laser-blink.py
@@ -2,13 +2,13 @@ from time import sleep
 
 import pigpio
 
-pi = pigpio.pi('10.42.0.157')
+pi = pigpio.pi('10.42.0.157')  # PI host where pigpiod is running
 
 PIN = 17  # Signal pin of a laser, BCM numeration
 pi.set_mode(PIN, pigpio.OUTPUT)
 
 while True:
-    pi.write(17, 1)
+    pi.write(PIN, 1)
     sleep(1)
-    pi.write(17, 0)
+    pi.write(PIN, 0)
     sleep(1)
